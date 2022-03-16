7fe5a45ce86ca0a8296a90d9e132407834a328be
sangeonpark <parksang1324@gmail.com>
2022-03-16 10:48:20 +0900

    3/16 event#1

--- kanban/src/CardList.js
+++ kanban/src/CardList.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import styles from './assets/css/CardList.css'
-const CardList = ({title, Card}) => { // 맵 사용해서 ㄱㄱ
+const CardList = ({title, cards}) => { // 맵 사용해서 ㄱㄱ
+  console.log(title,cards);
   return (
     <div className={styles.CardList}>
         <h1>{title}</h1>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-
+        
     </div>
 
   )
--- kanban/src/KanbanBoard.js
+++ kanban/src/KanbanBoard.js
@@ -4,12 +4,16 @@ import styles from './assets/css/KanbanBoard.css'
 import cards from './data.json'
 
 const KanbanBoard = () => {
-  console.log(cards)
+  // console.log(cards)
+
+  // const result = [0,1,2,3,4].filter(e =>e%2===0);
+
+  // console.log(result)
   return (
     <div className={styles.KanbanBoard}>
-        <CardList title = {'ToDo'} cards={} />   
-        <CardList title = {'Doing'}/>   
-        <CardList title = {'Done'}/>   
+        <CardList title = {'ToDo'} cards={cards.filter(e => e.status ==='ToDo')}/>   
+        <CardList title = {'Doing'} cards={cards.filter(e => e.status ==='Doing')}/>   
+        <CardList title = {'Done'} cards={cards.filter(e => e.status ==='Done')}/>   
     </div>
   )
 }
