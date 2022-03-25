f900c6ce2ab568f4bba16b53a26434643c89dff9
sangeonpark <parksang1324@gmail.com>
2022-03-25 09:55:22 +0900

    3/25 ticks

--- 2.Component/ex05/src/04/App.js
+++ 2.Component/ex05/src/04/App.js
@@ -3,7 +3,7 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
-    const [date,setDate] = useState(new Date())
+    const [date,setDate] = useState(new Date)
     const [ticks,setTicks] = useState(0)
     
     const getCurrentClockTime = () => {
@@ -11,20 +11,35 @@ export default function App() {
     };
 
     useEffect(() => {
-        setInterval(function(){
-           
+        const i = ticks
+        setTimeout(function(){
+           setTicks(i+1)
             getCurrentClockTime();
         },1000)
         
-    },[]);
+    },[getCurrentClockTime]);
+
+        
+
+    // useEffect(() => {
+    //     const i = ticks
+    //     setInterval(function(){
+    //        setTicks(i+1)
+    //         getCurrentClockTime();
+    //     },1000)
+        
+    // },[]);
 
     return (
+            <div>
+            <span>{ticks}</span>
             <Clock
                 message={'ex05: useEffect Hook example'}
                 hours={date.getHours()}
                 minutes={('0'+date.getMinutes()).slice(-2)}
                 seconds={('0'+date.getSeconds()).slice(-2)}
                 />
+                </div>
     );
 
     
