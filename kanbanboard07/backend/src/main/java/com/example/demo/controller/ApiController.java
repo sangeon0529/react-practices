63dc2ad2cc4251671c33e5dbbff1e203f4bb4b0a
sangeonpark <parksang1324@gmail.com>
2022-03-28 20:19:25 +0900

    kanban card

--- kanbanboard07/backend/src/main/java/com/example/demo/controller/ApiController.java
+++ kanbanboard07/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -8,13 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.JsonResult;
+import com.example.demo.repository.KanbanboardRepository;
+
+
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 	
+	@Autowired
+	private KanbanboardRepository kanbanboardRepository;
 
+	@GetMapping("")
+	public ResponseEntity<JsonResult> read() {
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(kanbanboardRepository.findAll()));
+	}
 }
--- kanbanboard07/frontend/src/KanbanBoard.js
+++ kanbanboard07/frontend/src/KanbanBoard.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import CardList from './CardList'
 import styles from './assets/css/KanbanBoard.css'
+import { useState } from 'react'
 
 const KanbanBoard = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(async () => {
+    try {  
+      const response = await fetch('/api', {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: null
+      });
+
+      if(!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+
+      if(json.result !== 'success') {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+      
+      setCards(json.data);
+    } catch(err) {
+      console.log(err);      
+    }  
+  }, []);
   // console.log(cards)
 
   // const result = [0,1,2,3,4].filter(e =>e%2===0);
