b133f435a963bc827e2b28f39527ab3a824add55
sangeonpark <parksang1324@gmail.com>
2022-03-31 11:43:41 +0900

    router styling

--- 3.routing/src/05/layout/Header.js
+++ 3.routing/src/05/layout/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../assets/scss/layout/Header.scss';
-
+import {Link, NavLink} from 'react-router-dom'
 export default function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <h1><Link to={'/'}> Header </Link></h1>
         </header>
     );
 }
