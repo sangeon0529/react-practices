f67ecf608ba7440bf664f10388fa070f9996f4de
sangeonpark <parksang1324@gmail.com>
2022-04-04 12:34:02 +0900

    publish #4

--- emaillist07/frontend/src/SearchBar.js
+++ emaillist07/frontend/src/SearchBar.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import styles from './assets/scss/SearchBar.scss'
-import PropTypes from 'prop-types'
+import styles from './assets/scss/SearchBar.scss';
+import PropTypes from 'prop-types';
 
-export default function SearchBar({callback})  {
+export default function SearchBar({callback}) {
   return (
     <div className={styles.Searchbar}>
-        <input 
+        <input
           type='text'
           placeholder='찾기'
           onChange={e => callback(e.target.value)} />
